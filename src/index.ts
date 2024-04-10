2db5cb59415f4f8747252bab55c352a5ff967ee9
Eien Mojiki 🍕 <95165750+eienmojiki206@users.noreply.github.com>
2024-04-10 16:23:47 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,46 +20,60 @@ export default async function createQuoteImage(
   await nodeHtmlToImage({
     output: out,
     html: `<html>
-        <head>
-          <link rel="preconnect" href="https://fonts.googleapis.com">
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-          <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;1,700&display=swap" rel="stylesheet">
-          <style>
+        <head><html>
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&amp;display=swap" rel="stylesheet">
+        <style>
             body {
-              font-family: 'Lato', sans-serif;
-              width: 900px;
-              height: 1600px;
-              display: flex;
-              flex-direction: column;
-              justify-content: space-between;
-              align-items: center;
-              text-align: center;
-              font-weight: 400;
-              padding: 2em;
-              background-color: ${background};
+                font-family: 'Source Code Pro', sans-serif;
+                width: 1600px;
+                height: 9000px;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                align-items: center;
+                text-align: center;
+                font-weight: 400;
+                padding: 2em;
+                background-color: ${background};
             }
+
             h1 {
-              font-size: 5em;
-              color: ${textColor};
-              line-height: 1.5;
-              font-weight: 700;
+                font-size: 4em;
+                line-height: 1.5;
+                font-weight: 700;
+                color: ${textColor};
             }
+
+            h1::before {
+                content: '"';
+                color: ${authorColor};
+            }
+
+            h1::after {
+                content: '"';
+                color: ${authorColor};
+            }
+
             h2 {
-              font-size: 4em;
-              font-style: italic;
-              color: ${authorColor};
+                font-size: 3em;
+                font-style: italic;
+                color: ${authorColor};
             }
-          </style>
-        </head>
-        <body>
-          <div></div>
-          <div>
+        </style>
+    </head>
+
+    <body>
+        <div></div>
+        <div>
             <h1>${text}</h1>
             <h2>${author}</h2>
-          </div>
-          <div></div>
-        </body>
-      </html>
+        </div>
+        <div></div>
+    </body>
+</html>
       `,
     puppeteerArgs: {
       headless: true,
